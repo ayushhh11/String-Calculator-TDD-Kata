52215a4842817e2d9a1ef35f94a2e9fb61843384
Ayush <ayushsinghtomar1@gmail.com>
2025-01-03 23:01:05 +0530

    feat: added test cases with multiple numbers along with delimiters in between.
    Using regex expression, such that it will allow only digits.
    
    Once only digits are there, traversing them to get the total sum

--- calculator.js
+++ calculator.js
@@ -2,5 +2,13 @@ export const sum = (a)=> {
     if(a==''){
         return 0;
     }
+    a= a.replace(/\D/g,'')
+    let number_array = a.split("")
+    let total = 0
+    for(let number of number_array){
+        total = total+ Number(number)
+    }
+    return total
+
 }
 
--- calculator.test.js
+++ calculator.test.js
@@ -5,4 +5,22 @@ describe("Calculator tdd",()=>{
 test('empty string sum',()=>{
     expect(sum('')).toBe(0);
 })
+
+test('sum of one number',()=>{
+    expect(sum('1')).toBe(1);
+})
+
+test('sum of 2 and 3 should be 5', () => {
+    expect(sum('2,3')).toBe(5);
+});
+
+test('sum using different delimiters', () => {
+    expect(sum(';\n1;2')).toBe(3);
+});
+
+test('sum using different delimiters', () => {
+    expect(sum(';\n1;2')).toBe(3);
+});
+
+
 });
