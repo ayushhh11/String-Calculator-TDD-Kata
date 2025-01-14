1d07876702398e885ef51ab26bf09eae0ba65b0c
Ayush <ayushsinghtomar1@gmail.com>
2025-01-14 23:55:34 +0530

    fix: test cases Added test cases for the negative numbers

--- calculator.js
+++ calculator.js
@@ -1,17 +1,14 @@
-export const sum = (string)=> {
-    if(string==''){
+export const sum = (string) => {
+    if (string === '') {
         return 0;
     }
-    if(string.includes('-')){
-        return "negative numbers not allowed"
-    }
-    string= string.replace(/\D/g,'')
-    let number_array = string.split("")
-    let total = 0
-    for(let number of number_array){
-        total = total+ Number(number)
-    }
-    return total
 
-}
+    let d = /,|\n/;
+    if (string.startsWith("//")) {
+        const parts = string.split("\n");
+        d = new RegExp(parts[0].slice(2));
+        string = parts.slice(1).join("\n"); 
+    }
+
+};
 
